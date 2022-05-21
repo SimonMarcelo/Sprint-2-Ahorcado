4644e58f5fc99f90edadb997beab56da6fbdad44
Marcelo Simon <marcelo.simon@gmail.com>
2022-05-21 13:12:41 -0300

    juego terminado

--- js/botones.js
+++ js/botones.js
@@ -1,69 +1,64 @@
 var palabraIngresada = document.querySelector("textarea");
 
-function agregar(palabraIngresada){
-    if (!palabrasSecretas.includes(palabraIngresada.toUpperCase()) && palabraIngresada!=""){
+function agregar(palabraIngresada) {
+    if (
+        !palabrasSecretas.includes(palabraIngresada.toUpperCase()) &&
+        palabraIngresada != ""
+    ) {
         palabrasSecretas.push(palabraIngresada.toUpperCase());
     }
 }
 
-function mostrarInicio(){
+function mostrarInicio() {
     pantallaInicio.classList.remove("no-display");
     pantallaAgregarPalabra.classList.add("no-display");
     pantallaJuego.classList.add("no-display");
 }
 
-function mostrarAgregarPalabra(){
+function mostrarAgregarPalabra() {
     pantallaInicio.classList.add("no-display");
     pantallaAgregarPalabra.classList.remove("no-display");
     pantallaJuego.classList.add("no-display");
 }
 
-function mostrarJugar(){
+function mostrarJugar() {
     pantallaInicio.classList.add("no-display");
     pantallaAgregarPalabra.classList.add("no-display");
     pantallaJuego.classList.remove("no-display");
 }
 
 var nuevoJuego = document.querySelector("#boton-iniciar-juego");
-nuevoJuego.addEventListener("click",function(){
+nuevoJuego.addEventListener("click", function () {
     mostrarJugar();
     juegoNuevo();
 });
 
 var agregarPalabra = document.querySelector("#boton-agregar-palabra");
-agregarPalabra.addEventListener("click",function(){
+agregarPalabra.addEventListener("click", function () {
     mostrarAgregarPalabra();
     palabraIngresada.value = "";
 });
 
 var guardarYEmpezar = document.querySelector("#boton-guardar-y-empezar");
-guardarYEmpezar.addEventListener("click",function(){
+guardarYEmpezar.addEventListener("click", function () {
     agregar(palabraIngresada.value);
     mostrarJugar();
     juegoNuevo();
 });
 
 var cancelar = document.querySelector("#boton-cancelar");
-cancelar.addEventListener("click",function(){
+cancelar.addEventListener("click", function () {
     mostrarInicio();
 });
 
-// var nuevoJuego = document.querySelector("#boton-nuevo-juego");
-// var letrasDeLaPalabra = document.querySelector("#palabra-secreta").innerHTML;
-// var guiones = document.querySelector("#guiones");
-// nuevoJuego.addEventListener("click",function(){
-//     elegirPalabraSecreta();
-    // window.location.reload("Refresh");
-    // letrasDeLaPalabra.value =  letrasDePalabraSecreta.replaceAll("a","*");
-    // guiones.value = letrasDePalabraSecreta.replaceAll(letras,"_");
-    // letrasDeLaPalabra.value = palabraSecreta;
-    // }
-    
-// });
+var nuevoJuego = document.querySelector("#boton-nuevo-juego");
+nuevoJuego.addEventListener("click", function () {
+    juegoNuevo();
+    mostrarJugar();
+});
 
 var desistir = document.querySelector("#boton-desistir");
 var imagenDerrota = document.querySelector("#imagen-derrota");
-desistir.addEventListener("click",function(){
-    imagenDerrota.classList.remove("no-display");
+desistir.addEventListener("click", function () {
+    mostrarPerdedor();
 });
-
--- js/juego.js
+++ js/juego.js
@@ -1,68 +1,150 @@
 var jugando = false;
-var palabrasSecretas = ['ALURA','HTML','CANVAS','ARRAY','LOOP','OBJETO','WEB','CONSOLE','PUSH','LENGTH','GITHUB','COMPUTADORA'];
-const letras = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','Ñ','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-var letrasErradas = [];
+var palabrasSecretas = [
+    "ALURA",
+    "HTML",
+    "CANVAS",
+    "ARRAY",
+    "LOOP",
+    "OBJETO",
+    "WEB",
+    "CONSOLE",
+    "PUSH",
+    "LENGTH",
+    "GITHUB",
+    "ORACLE",
+];
+var myModal = new bootstrap.Modal(document.getElementById("modal"), {});
+var myModal2 = new bootstrap.Modal(document.getElementById("modal-ganaste"), {});
+var letrasEquivocadas = [];
 var letrasUsadas = [];
 var palabraSecreta;
+const cantidadDeErrores = 9;
 var pantallaInicio = document.querySelector("#inicio");
 var pantallaAgregarPalabra = document.querySelector("#agregar-palabra");
 var pantallaJuego = document.querySelector("#juego");
 var palabraOculta = document.querySelector("#palabra-secreta");
-var guiones = document.querySelector("#guiones");
 var selectorDePalabra = document.querySelector("#palabra");
 
+var palabraSecretaArray = [];
 
-function elegirPalabraSecreta(){
+function elegirPalabraSecreta() {
     palabraSecreta = palabrasSecretas[Math.round(Math.random() * (palabrasSecretas.length - 1))];
+
     return palabraSecreta;
 }
 
-elegirPalabraSecreta();
-console.log(palabraSecreta);
-
-
-var errores = 0;
-;
-
-function juegoNuevo(){
-    jugando = true;
-    console.log("inicio de juego");
+function juegoNuevo() {
+    estadoInicial();
     elegirPalabraSecreta();
     for (let index = 0; index < palabraSecreta.length; index++) {
         const element = palabraSecreta[index];
-        selectorDePalabra.innerHTML +=  generadorHTMLLetra('');
+        palabraSecretaArray.push({ letra: element, estado: false });
+        selectorDePalabra.innerHTML += generadorHTMLLetra("");
     }
-        
 }
 
-function generadorHTMLLetra(letra){
+function generadorHTMLLetra(letra) {
     return `<div class="letra row"><span>${letra}</span><div></div></div>`;
 }
 
-
-
-// Array.from(palabraSecreta)
-
-function adivinarLetra(inputLetra){
+function adivinarLetra(inputLetra) {
     let letraEncontrada = false;
     for (let index = 0; index < palabraSecreta.length; index++) {
-        if(palabraSecreta[index] == inputLetra.toUpperCase()){
-            document.getElementsByClassName("letra")[index].firstElementChild.innerText = palabraSecreta[index];
+        if (palabraSecreta[index] == inputLetra.toUpperCase()) {
+            document.getElementsByClassName("letra")[index].firstElementChild.innerText =
+                palabraSecreta[index];
             letraEncontrada = true;
+            palabraSecretaArray[index]["estado"] = true;
         }
     }
     return letraEncontrada;
 }
 
-document.addEventListener("keyup",function(event){
-    if(!jugando && !/[a-zA-Z]/.test(event.key) && event.key != "Enter" && letrasUsadas.indexOf(event.key) >= 0){
+document.addEventListener("hidden.bs.modal", function (event) {
+    juegoNuevo();
+    mostrarJugar();
+});
+
+document.addEventListener("keyup", function (event) {
+    let tecla = event.key;
+    if (
+        !jugando ||
+        !/[a-zA-Z]/.test(tecla) ||
+        tecla == "Enter" ||
+        letrasUsadas.indexOf(tecla) >= 0
+    ) {
         return false;
     }
-    letrasUsadas.push(event.key)
-    if (!adivinarLetra(event.key)){
-        letrasErradas.toUpperCase.push(event.key);
-        // dibujarLetraErrada(event.key)
-        // dibujar
+    letrasUsadas.push(tecla);
+    let adivinaLetra = adivinarLetra(tecla);
+    if (!adivinaLetra && letrasEquivocadas.indexOf(tecla) < 0) {
+        letrasEquivocadas.push(tecla);
+        imprimirArrayayEnH1();
+        mostrarParte(letrasEquivocadas.length - 1);
+        if (revisarSiPerdio()) {
+            mostrarPerdedor();
+        }
+    } else if (adivinaLetra && revisarSiGano()) {
+        mostrarGanador();
     }
-    console.log(event.key);
 });
+
+function imprimirArrayayEnH1() {
+    let erradas = document.querySelector("#letras-erradas");
+    var span = document.createElement("span");
+    span.appendChild(document.createTextNode(letrasEquivocadas[letrasEquivocadas.length - 1]));
+    span.setAttribute("class", "ps-3");
+    erradas.appendChild(span);
+}
+
+function revisarSiGano() {
+    let resultado = palabraSecretaArray.filter((letra) => letra.estado == false);
+    return resultado.length == 0;
+}
+
+function revisarSiPerdio() {
+    return letrasEquivocadas.length > cantidadDeErrores;
+}
+
+function mostrarParte(parte) {
+    let dibujo = document.querySelector("svg").children[parte];
+    dibujo.classList.remove("no-display");
+}
+
+function ocultarDibujo() {
+    let dibujo = document.querySelector("svg").children;
+    // dibujo.classList.add("no-display");
+    for (let index = 0; index < dibujo.length; index++) {
+        const element = dibujo[index];
+        element.classList.add("no-display");
+    }
+}
+
+function mostrarPerdedor() {
+    const mensajeModalPerdedor = document
+        .getElementById("modal")
+        .getElementsByClassName("modal-body")[0]
+        .getElementsByTagName("p")[0];
+    mensajeModalPerdedor.innerHTML = "LA PALABRA ERA " + palabraSecreta;
+    myModal.show();
+}
+
+function mostrarGanador() {
+    const mensajeModalGanador = document
+        .getElementById("modal-ganaste")
+        .getElementsByClassName("modal-body")[0]
+        .getElementsByTagName("p")[0];
+    mensajeModalGanador.innerHTML = "¡¡¡HICISTE UN EXCELENTE TRABAJO!!! <br> LA PALABRA ERA <b>" + palabraSecreta + "</b>";
+    myModal2.show();
+}
+
+function estadoInicial() {
+    let erradas = document.querySelector("#letras-erradas");
+    erradas.innerHTML = "";
+    jugando = true;
+    selectorDePalabra.innerHTML = "";
+    letrasUsadas = [];
+    letrasEquivocadas = [];
+    palabraSecretaArray = [];
+    ocultarDibujo();
+}
