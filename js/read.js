a9a8299b29928d169ddac854281d280ea5dbd600
Marcelo Simon <marcelo.simon@gmail.com>
2022-05-26 22:56:04 -0300

    guardan palabras de usuario utilizando localStorage

--- js/read.js
+++
@@ -1,9 +0,0 @@
-import { readFile } from "fs";
-
-readFile("palabrasRandom.txt", "utf8", function (err, data) {
-    if (err) {
-        return console.log(err);
-    }
-
-    console.log(data);
-});
